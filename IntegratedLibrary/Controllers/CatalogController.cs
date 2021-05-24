9acd7bef2a2771b02bd9af2fe8fe06557523e330
sumitra-source <shresthasumitra1998@gmail.com>
2021-05-24 21:50:29 +0545

    24/5

--- IntegratedLibrary/Controllers/CatalogController.cs
+++ IntegratedLibrary/Controllers/CatalogController.cs
@@ -8,9 +8,11 @@ namespace IntegratedLibrary.Controllers
     public class CatalogController:Controller
     {
         private ILibraryAsset _assets;
-        public CatalogController(ILibraryAsset assets)
+        private ICheckout _checkouts;
+        public CatalogController(ILibraryAsset assets,ICheckout checkouts)
         {
             _assets = assets;
+            _checkouts=checkouts;
         }
         public IActionResult Index()
         {
@@ -35,19 +37,30 @@ namespace IntegratedLibrary.Controllers
         public IActionResult Detail(int id)
         {
             var asset = _assets.GetById(id);
+            var currentHolds = _checkouts.GetCurrentHolds(id)
+                .Select(a => new AssetHoldModel
+                {
+                    HoldPlaced = _checkouts.GetCurrentHoldPlaced(a.Id).ToString("d"),
+                    PatronName = _checkouts.GetCurrentHoldPatronName(a.Id)
+                });
             var model = new AssetDetailModel
             {
                 AssetId = id,
                 Title = asset.Title,
+                Type = _assets.GetType(id),
                 Year = asset.Year,
                 Cost = asset.Cost,
                 Status = asset.Status.Name,
                 ImageUrl = asset.ImageUrl,
                 AuthorOrDirector = _assets.GetAuthorOrDirector(id),
-                CurrentLocation=_assets.GetCurrentLocation(id).Name,
+                CurrentLocation = _assets.GetCurrentLocation(id).Name,
                 DeweyCallNumber = _assets.GetDeweyIndex(id),
-                
+                CheckoutHistory = _checkouts.GetCheckoutHistory(id),
+
                 ISBN = _assets.GetIsbn(id),
+                LatestCheckout = _checkouts.GetLatestCheckout(id),
+                PatronName=_checkouts.GetCurrentCheckoutPatron(id),
+                CurrentHolds=currentHolds
 
             };
             return View(model);
